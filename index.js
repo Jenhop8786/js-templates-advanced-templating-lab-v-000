de991b89c7ece042c9733edc636ea38ff1d1b22d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-19 18:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  document.getElementByTagName("main")[0].innerHTML += recipeFormTemplate({'onsubmit': 'createRecipe();'}) Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-template").innerHTML)
+   Handlebars.registerHelper('displayIngredient', function() {
+     if(this != "") {
+       return new Handlebars.Safestring('<li class="recipeIngredients">` + this + `</li>` )
+     }
+   })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
