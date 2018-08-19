e1f95ae40d578907bae81e080535491423517e19
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-19 18:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,16 @@ function createRecipe() {
   for(let i = 0; i < ingredientsValue.length; i++) {
     ingredients.push(ingredientsValues[i].value)
   }
+
+  let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+   document.getElementsByTagName("main")[0].innerHTML += recipeTemplate({'name': name, 'description': description, 'ingredients': ingredients})
+}
+
+function displayEditForm() {
+  let recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  document.getElementsByTagName("main")[0].innerHTML += recipeFormTemplate()
+}
+
+function updateRecipe() {
+  createRecipe()
 }
